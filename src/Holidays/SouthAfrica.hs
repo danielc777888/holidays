90d0b1c5ed08a65070cd95f10ac9f353a75f4940
daniel-dev <danielc777888@gmail.com>
2024-12-02 08:54:43 +0200

    use latest ghc

--- src/Holidays/SouthAfrica.hs
+++ src/Holidays/SouthAfrica.hs
@@ -1,15 +1,21 @@
-module Holidays.SouthAfrica
-(holidays,
- Holiday(..)
-  )
-where
+module Holidays.SouthAfrica (
+  holidays,
+  Holiday(..)
+) where
+
+-- holidays :: Country -> [Year] -> [Holiday]
+
+-- holidays :: [Year] -> [Holiday]
 
 import Data.Time
 
-data Holiday = Holiday {
-  day        :: Day,
-  name       :: String,
-  instituted :: Int } deriving Show
+data Holiday = Holiday
+                 { day        :: Day
+                 , month      :: Int
+                 , name       :: String
+                 , instituted :: Int
+                 }
+     deriving (Show)
 
 minYear :: Int
 minYear = 1910
@@ -22,9 +28,11 @@ years = [minYear..maxYear]
 
 workersDay :: Holiday
 workersDay = Holiday {
-  day = fromGregorian 0 5 1,
-  name = "Workers Day",
-  instituted = 1995 }
+    day = fromGregorian 0 5 1,
+    month = 1,
+    name = "Workers Day",
+    instituted = 1995
+  }
 
 holidays :: [Holiday]
 holidays = concat (go years)
@@ -34,10 +42,11 @@ holidays = concat (go years)
           | x >= instituted workersDay = [holiday x workersDay]: go xs
           | otherwise = go xs
 
-
 holiday :: Int -> Holiday -> Holiday
-holiday year h = Holiday { day = day', name = name', instituted = instituted' }
-  where (_, m, d) = toGregorian (day h)
+holiday year h = Holiday { day = day', month = 1, name = name', instituted = instituted' }
+  where (_, m, d) = toGregorian (fromGregorian 0 5 1)
         day' = fromGregorian (toInteger year) m d
         name' = name h
         instituted' = instituted h
+
+       
