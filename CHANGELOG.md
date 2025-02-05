d0f060ead92f239bbb1e8f0eff734f1c822c36a1
daniel-dev <danielc777888@gmail.com>
2025-02-05 22:40:54 +0200

    update README, added more code docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,6 @@
 
 ## **0.1.0.0** - 2025-02-04
 - small EDSL for describing holidays
-- ZAF, NAM, USA, GBR
+- ZAF, NAM, USA, GBR, DEU
+- support for country regions
 - unit + property based tests
--- src/Holidays/Base.hs
+++ src/Holidays/Base.hs
@@ -7,10 +7,10 @@ module Holidays.Base (
 import Data.Text qualified as T
 import Data.Time
 
--- 3-letter country codes
+-- | 3-letter country codes
 type ISO_3166_1_Alpha_3 = T.Text
 
--- Custom regions for a country
+-- | Custom regions for a country
 type Region = T.Text
 
 day :: Year -> MonthOfYear -> DayOfMonth -> Day
